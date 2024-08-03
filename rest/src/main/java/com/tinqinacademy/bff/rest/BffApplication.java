4f74db9ec5d6909fdef11fbb2739cbfc515453fe
IvanDimovSIT <ivan390@abv.bg>
2024-08-03 15:12:51 +0300

    Removed database access

--- rest/src/main/java/com/tinqinacademy/bff/rest/BffApplication.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/BffApplication.java
@@ -2,14 +2,12 @@ package com.tinqinacademy.bff.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+
 @ComponentScan(basePackages = "com.tinqinacademy.bff")
-@EnableJpaRepositories(basePackages = "com.tinqinacademy.bff.persistence.repository")
-@EntityScan(basePackages = "com.tinqinacademy.authentication.persistence.model")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BffApplication {
 
     public static void main(String[] args) {
