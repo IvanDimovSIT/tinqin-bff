aefeaaa9d4be4c2db6ca175c12c051d7b705a08a
Ivan Dimov <100278096+IvanDimovSIT@users.noreply.github.com>
2024-08-12 22:37:42 +0300

    [T12] Fixed secured url paths (#24)
    Fixed conflict in spring security where two or more operations with different methods share the same url.

--- rest/src/main/java/com/tinqinacademy/bff/rest/security/SecurityConfig.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/security/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.tinqinacademy.bff.rest.security;
 import com.tinqinacademy.bff.api.RestApiRoutes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -15,34 +17,30 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    private final String[] ADMIN_URLS = {
-            RestApiRoutes.SYSTEM_DELETE_ROOM,
-            RestApiRoutes.SYSTEM_ADD_ROOM,
-            RestApiRoutes.SYSTEM_PARTIAL_UPDATE_ROOM,
-            RestApiRoutes.SYSTEM_UPDATE_ROOM,
-            RestApiRoutes.SYSTEM_REGISTER_VISITOR,
-            RestApiRoutes.HOTEL_GET_ROOM,
-            RestApiRoutes.SYSTEM_ADMIN_DELETE_COMMENT,
-            RestApiRoutes.SYSTEM_ADMIN_EDIT_COMMENT,
-            RestApiRoutes.SYSTEM_GET_VISITORS,
-    };
-    private final String[] USER_URLS = {
-            RestApiRoutes.HOTEL_ADD_COMMENT,
-            RestApiRoutes.HOTEL_BOOK_ROOM,
-            RestApiRoutes.HOTEL_UNBOOK_ROOM,
-            RestApiRoutes.HOTEL_EDIT_COMMENT,
-
-
-    };
-    //private static final String[] PUBLIC_URLS = {
-    //        "/swagger-ui.html",
-    //        "/swagger-ui/**",
-    //        "/v3/api-docs/**",
-    //        "/webjars/**"
-    //};
 
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    private void configureAdminAccess(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry request) {
+        request
+                .requestMatchers(HttpMethod.POST, RestApiRoutes.SYSTEM_REGISTER_VISITOR).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.GET, RestApiRoutes.SYSTEM_GET_VISITORS).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, RestApiRoutes.SYSTEM_DELETE_ROOM).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, RestApiRoutes.SYSTEM_ADD_ROOM).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, RestApiRoutes.SYSTEM_PARTIAL_UPDATE_ROOM).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.PUT, RestApiRoutes.SYSTEM_UPDATE_ROOM).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, RestApiRoutes.SYSTEM_ADMIN_DELETE_COMMENT).hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.PUT, RestApiRoutes.SYSTEM_ADMIN_EDIT_COMMENT).hasAuthority("ADMIN");
+
+    }
+
+    private void configureUserAccess(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry request) {
+        request
+                .requestMatchers(HttpMethod.POST, RestApiRoutes.HOTEL_BOOK_ROOM).hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE, RestApiRoutes.HOTEL_UNBOOK_ROOM).hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, RestApiRoutes.HOTEL_ADD_COMMENT).hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers(HttpMethod.PATCH, RestApiRoutes.HOTEL_EDIT_COMMENT).hasAnyAuthority("USER", "ADMIN");
     }
 
     @Bean
@@ -50,9 +48,8 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> {
-                    request.requestMatchers(ADMIN_URLS).hasAuthority("ADMIN");
-                    request.requestMatchers(USER_URLS).hasAnyAuthority("USER", "ADMIN");
-                    //request.requestMatchers(PUBLIC_URLS).permitAll();
+                    configureAdminAccess(request);
+                    configureUserAccess(request);
                     request.anyRequest().permitAll();
                 })
                 .sessionManagement(
